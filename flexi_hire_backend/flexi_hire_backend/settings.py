c08d3af3ee64927600abd0c7a541ac1a2942787e
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-07-24 13:36:27 +0300

    updating secret key path

--- flexi_hire_backend/flexi_hire_backend/settings.py
+++ flexi_hire_backend/flexi_hire_backend/settings.py
@@ -128,7 +128,9 @@ import os
 import json
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-FIREBASE_CREDENTIALS = os.path.join(BASE_DIR, '..', 'flexi-hire-firebase-adminsdk-fbsvc-40e6aaee3f.json')
+# FIREBASE_CREDENTIALS = os.path.join(BASE_DIR, '..', 'flexi-hire-firebase-adminsdk-fbsvc-40e6aaee3f.json')
+FIREBASE_CREDENTIALS = '/etc/secrets/firebase-credentials.json'
+
 
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
